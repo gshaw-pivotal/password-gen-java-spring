dcacc8385caf93addfdafc1045dd81c71af46607
Gavin Shaw <gavshaw+github@gmail.com>
2024-07-19 11:34:46 -0400

    Base springboot app structure

---
+++ src/main/java/com/gs/password_gen_java_spring/PasswordGenJavaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.gs.password_gen_java_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordGenJavaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordGenJavaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gs/password_gen_java_spring/PasswordGenJavaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gs.password_gen_java_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordGenJavaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
